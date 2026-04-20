638a38c8856a0c47fed695163a9cf5c2b1130d04
Mercado-9S-ay2526 <mercadosancholl@gmail.com>
2026-04-21 07:44:01 +0800

    update main

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function openGate() {
-let  carCounter = 18;
-const maxCars = 25;
+let  carCounter = 10;
+const maxCars = 1000;
 
 while (carCounter < maxCars) {
 window.alert("Car No. " + carCounter + " is entering");
     carCounter += 1;
 
 }
+window.alert("The Garage is full at 1000 cars.");
 }
